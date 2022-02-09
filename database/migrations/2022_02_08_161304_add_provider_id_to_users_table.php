85280f8c69036308a7d761858dc18cd2fab53be9
rizmoo <morrismune.mm@gmail.com>
2022-02-09 21:49:58 +0300

    jj

--- database/migrations/2022_02_08_161304_add_provider_id_to_users_table.php
+++ database/migrations/2022_02_08_161304_add_provider_id_to_users_table.php
@@ -14,7 +14,7 @@ class AddProviderIdToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('provider_id')->nullable();
+//            $table->unsignedBigInteger('provider_id')->nullable();
         });
     }
 
